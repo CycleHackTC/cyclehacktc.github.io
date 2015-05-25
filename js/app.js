c4a37266fd64ed93816dc92dadbc26d83ad838fd
Alan Palazzolo <apalazzolo@minnpost.com>
2015-05-25 17:40:08 -0500

    Adding more details.

--- js/app.js
+++ js/app.js
@@ -5,4 +5,23 @@ $(document).ready(function() {
   // Attach fastclick
   FastClick.attach(document.body);
 
+  // Get width
+  var width = $(window).width();
+
+  // Maps
+  var hackMap = L.map('map-hack-factory', {
+    attributionControl: false,
+    zoomControl: false,
+    scrollWheelZoom: false,
+    touchZoom: false,
+    dragging: false
+  });
+  hackMap.setView([44.955389, -93.226018], 14);
+  L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+
+  }).addTo(hackMap);
+
+  var hackMarker = L.marker([44.955389, -93.226018]).addTo(hackMap);
+
+  hackMap.panBy([width / 4, 0]);
 });
